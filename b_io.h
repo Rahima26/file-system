af89d7fe68c9b9a90ff781ff59c795b257c461f7
Rahima26 <70996075+Rahima26@users.noreply.github.com>
2023-01-13 16:35:32 -0800

    basic i/o functions

---
+++ b_io.h
@@ -0,0 +1,28 @@
+/**************************************************************
+* Class:  CSC-415-0#  Fall 2021
+* Names: 
+* Student IDs:
+* GitHub Name:
+* Group Name:
+* Project: Basic File System
+*
+* File: b_io.h
+*
+* Description: Interface of basic I/O functions
+*
+**************************************************************/
+
+#ifndef _B_IO_H
+#define _B_IO_H
+#include <fcntl.h>
+
+typedef int b_io_fd;
+
+b_io_fd b_open (char * filename, int flags);
+int b_read (b_io_fd fd, char * buffer, int count);
+int b_write (b_io_fd fd, char * buffer, int count);
+int b_seek (b_io_fd fd, off_t offset, int whence);
+void b_close (b_io_fd fd);
+
+#endif
+
